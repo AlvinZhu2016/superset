9aa3cf4c689e2bfd0664d196cf1e40df7b60c268
Mark Underseth <marku@s2technologies.com>
2017-04-15 09:09:17 -0700

    initial clone

---
+++ superset/assets/spec/javascripts/sqllab/ResultSet_spec.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { describe, it } from 'mocha';
+import { expect } from 'chai';
+import { Table } from 'reactable';
+
+import ResultSet from '../../../javascripts/SqlLab/components/ResultSet';
+import { queries } from './fixtures';
+
+describe('ResultSet', () => {
+  const mockedProps = {
+    query: queries[0],
+  };
+  it('renders', () => {
+    expect(React.isValidElement(<ResultSet />)).to.equal(true);
+  });
+  it('renders with props', () => {
+    expect(
+      React.isValidElement(<ResultSet />),
+    ).to.equal(true);
+  });
+  it('renders a Table', () => {
+    const wrapper = shallow(<ResultSet {...mockedProps} />);
+    expect(wrapper.find(Table)).to.have.length(1);
+  });
+});
